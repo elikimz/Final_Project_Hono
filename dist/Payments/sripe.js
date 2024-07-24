d0c1caccb280c63825de71c378946baf1e6c59db
elijah -kimani <elijahkimani1293@gmail.com>
2024-07-24 13:05:49 +0300

    commit

--- dist/Payments/sripe.js
+++ dist/Payments/sripe.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ClientURL = void 0;
-exports.ClientURL = "http://localhost:8000";
+exports.ClientURL = "http://localhost:5173";

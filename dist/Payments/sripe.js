4ebc4bde88b315ad2a88c2c24690bcc92bca7cb5
elijah -kimani <elijahkimani1293@gmail.com>
2024-07-24 11:17:44 +0300

    commit

--- dist/Payments/sripe.js
+++ dist/Payments/sripe.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ClientURL = void 0;
-exports.ClientURL = "https://final-project-hono.onrender.com";
+exports.ClientURL = "http://localhost:8000";

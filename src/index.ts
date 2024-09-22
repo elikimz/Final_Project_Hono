3a451d48384a11f0db9e6016c1e0b87149364414
elikimz <elijahkimani1293@gmail.com>
2024-09-22 12:11:13 +0300

    committ

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import 'dotenv/config';
-import { cors } from 'hono/cors';
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+ import "dotenv/config"
+ import { cors } from 'hono/cors'
 
-import { usersRouter } from './users/user.router';
-import { vehicleSpecificationsRouter } from './VehicleSpecifications/VehicleSpecifications.router';
-import { VehiclesRouter } from './vehicles/vihicle.router';
-import { LocationsRouter } from './Locations/Locations.router';
-import { BookingsRouter } from './Bookings/Bookings.router';
-import { PaymentsRouter } from './Payments/Payments.router';
-import { CustomerSupportTicketsRouter } from './CustomerSupportTickets/CustomerSupportTickets.router';
-import { FleetManagementRouter } from './FleetManagement/FleetManagement.router';
-import { authRouter } from './Authentication/Authenitication.router';
+import { usersRouter } from './users/user.router'
+import { vehicleSpecificationsRouter } from './VehicleSpecifications/VehicleSpecifications.router' 
+import { VehiclesRouter } from './vehicles/vihicle.router'  
+import{ LocationsRouter} from './Locations/Locations.router'
+import {BookingsRouter} from './Bookings/Bookings.router'
+import { PaymentsRouter} from './Payments/Payments.router' 
+import { CustomerSupportTicketsRouter} from './CustomerSupportTickets/CustomerSupportTickets.router'
+import { FleetManagementRouter} from './FleetManagement/FleetManagement.router'
+import { authRouter} from './Authentication/Authenitication.router'  
+ 
 
-const app = new Hono();
-
-app.use('/*', cors());
-
-// Assign unique base paths to each router
-app.route('/users', usersRouter);
-app.route('/vehicalspecification', vehicleSpecificationsRouter);
-app.route('/vehicles', VehiclesRouter);
-app.route('/locations', LocationsRouter);
-app.route('/bookings', BookingsRouter);
-app.route('/payments', PaymentsRouter);
-app.route('/support-tickets', CustomerSupportTicketsRouter);
-app.route('/fleet-management', FleetManagementRouter);
-app.route('/auth', authRouter);
+const app =new Hono()
+app.use('/*', cors())
+app.route("/", usersRouter)
+app.route("/",vehicleSpecificationsRouter)
+app.route("/",VehiclesRouter)
+app.route("/",LocationsRouter)
+app.route("/",BookingsRouter)
+app.route("/",PaymentsRouter)
+app.route("/", CustomerSupportTicketsRouter)
+app.route("/",FleetManagementRouter)
+app.route("/",authRouter)
 
 serve({
   fetch: app.fetch,
   port: Number(process.env.PORT)
-});
-
-console.log(`Server is running on port ${process.env.PORT}`);
+})
+console.log(`Server is running on port ${process.env.PORT}`)
